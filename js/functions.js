c5e8ed14aa0a9af831fca4d843275b98a247f90b
Михаил Седых <baran4ikov5@gmail.com>
2023-03-03 01:53:15 +0300

    добавляет функции

---
+++ js/functions.js
@@ -0,0 +1,60 @@
+const stringLength = (str, length) => {
+  let result = str.length <= length;
+  return result
+}
+
+stringLength('проверяемая строка', 20);
+stringLength('проверяемая строка', 18);
+stringLength('проверяемая строка', 10);
+
+const isPalindrome = (str) => {
+  let formattedStr = str.replaceAll(' ', '').toLowerCase();
+  let reversedStr = '';
+
+  for (let i = formattedStr.length - 1; i >= 0; i--) {
+      reversedStr += formattedStr[i];
+  }
+  return formattedStr === reversedStr;
+}
+
+isPalindrome('топот');
+isPalindrome('ДовОд');
+isPalindrome('Кекс');
+isPalindrome('Лёша на полке клопа нашёл ');
+
+const getNumbers = (param) => {
+  const str = param.toString();
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const current = parseInt(str[i], 10);
+    if (!isNaN(current)) {
+      result += current;
+    }
+  }
+  return (result === '') ? NaN : Number(result);
+};
+
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('а я томат');
+getNumber(2023);
+getNumber(-1);
+getNumber(1.5);
+
+function fillString(str, minLength, addSymbol) {
+  let i = 0;
+  let strSymbol = '';
+  while (str.length + strSymbol.length <= minLength) {
+    strSymbol += addSymbol[i];
+    i = i + 1 < addSymbol.length ? i + 1 : 0;
+  }
+  return strSymbol + str;
+}
+
+fillString('1', 2, '0');
+fillString('1', 4, '0');
+fillString('q', 4, 'werty');
+fillString('qwerty', 4, '0');
+fillString('q', 4, 'we');
